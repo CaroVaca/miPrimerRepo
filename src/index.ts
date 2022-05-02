aaa3e3e8c180f5bbe8445f46d14c18947e36b5ca
CaroVaca <104107454+CaroVaca@users.noreply.github.com>
2022-05-02 14:45:18 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+console.log("Torta Facil");
+console.log("Ingredientes");
+console.log("2 tazas de harina leudante");
+console.log("2 huevos");
+console.log("1/4 taza de aceite");
+console.log("1 taza de leche");
+console.log("1 taza de azucar");
+console.log("Procedimiento:");
+console.log("unir los huevos con el aceite, leche y azucar");
+console.log("batir hasta que quede bien integrada");
+console.log("agregar la harina suavemente");
+console.log("llevar a un horno bien caliente por 60 minutos");
